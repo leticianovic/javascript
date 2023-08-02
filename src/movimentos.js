23c5f2017fbfa7c9b9692abbf5fcd5bca7438acd
Letícia Silva <leticia.silva_2013@hotmail.com>
2023-08-02 09:47:34 -0300

    JavaScript Advanced: Replace, Match e Regex

--- src/movimentos.js
+++ src/movimentos.js
@@ -22,20 +22,33 @@ class Registro{
 
     resumo(){
         let valorAtualizado = 0
+        let tipo = ''
+
+        const regexB = new RegExp('bradesco', 'i') //Expressões regulares
+        const regexN = new RegExp('nubank', 'i')
+
         this.movimentacoes.forEach(lancamento => {
-            valorAtualizado += lancamento.saldo
+            lancamento.banco = lancamento.banco.replace(regexB, 'Nubank')
+            if(lancamento.banco.match(regexN)){
+                valorAtualizado += lancamento.saldo
+                tipo += lancamento.nome + ' | '
+            }
+            
         })
 
-        return valorAtualizado
+        return tipo + valorAtualizado
     }
 }
 
 const m01 = new Movimentacao('Nubank', 'salario', 5000)
-const m02 = new Movimentacao('Nubank', 'bonificação', 800)
+const m02 = new Movimentacao('Bradesco', 'bonificação', 800)
 const m03 = new Movimentacao('Nubank', 'imposto', -1000)
-const m04 = new Movimentacao('Nubank', 'contas', -3000)
+const m04 = new Movimentacao('Itau', 'contas', -3000)
+const m05 = new Movimentacao('Bradesco', 'imposto', -400)
+const m06 = new Movimentacao('Bradesco', 'salario', 8000)
+const m07 = new Movimentacao('Nubank', 'reembolso', 3000)
 
 const reg01 = new Registro(1,8,2023)
-reg01.novaMovimentacao(m01, m02, m03, m04)
+reg01.novaMovimentacao(m01, m02, m03, m04, m05, m06, m07)
 
 console.log('O saldo dos registros foi de: ' + reg01.resumo())
