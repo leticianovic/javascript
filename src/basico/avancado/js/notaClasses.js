98867f4e8260d49f58d70be2076b42f33c358fc8
Letícia Silva <leticia.silva_2013@hotmail.com>
2023-09-15 10:50:11 -0300

    Exercicio

--- src/basico/avancado/js/notaClasses.js
+++ src/basico/avancado/js/notaClasses.js
@@ -4,28 +4,33 @@ const nota3 = document.getElementsByName('n3');
 const nota4 = document.getElementsByName('n3');
 
 class Nota {
-    constructor(bimestre){
-        this.bimestre = bimestre
+    constructor(bimestre, nota){
+        this.bimestre = bimestre;
+        this.nota = nota;
     }
 
     bimestre1(){
-        console.log(this.bimestre + nota1)
+        this.nota = 10;
+        console.log(this.bimestre + this.nota)
     }
 
     bimestre2(){
+        this.nota = 9;
         console.log(this.bimestre + nota2)
     }
 
     bimestre3(){
+        this.nota = 2;
         console.log(this.bimestre + nota3)
     }
 
     bimestre4(){
+        this.nota = 4;
         console.log(this.bimestre + nota4)
     }
 }
 
-const resultadoNota = new Nota('Sua nota no ')
+const resultadoNota = new Nota('Sua nota no bimestre é: ');
 
 console.log(resultadoNota.bimestre1())
 console.log(resultadoNota.bimestre2())
